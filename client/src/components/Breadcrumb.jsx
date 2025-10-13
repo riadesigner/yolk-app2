c2cf490f22fd19faf1b5eb46941d7a7964f4df95
Roman Rachkov <pakycb84@gail.com>
2025-10-13 15:05:00 +1000

    [YOL-89] fix: Поправлена главная на странице авторизации

--- client/src/components/Breadcrumb.jsx
+++ client/src/components/Breadcrumb.jsx
@@ -1,19 +1,26 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function Breadcrumb({links}){    
-    return (
-        <nav className="breadcrumb" aria-label="breadcrumbs">            
-        <ul>
-            {
-                links.map((i, index)=>{
-                    return i.isActive?(
-                        <li key={index} className="is-active"><a>{i.title}</a></li>
-                    ):(
-                        <li key={index}><Link to={i.link}>{i.title}</Link></li>
-                    )
-                })
-            }
-        </ul>
-        </nav>
-    )
+export default function Breadcrumb({ links }) {
+  return (
+    <nav className="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        {links.map((i, index) => {
+          return i.isActive ? (
+            <li key={index} className="is-active">
+              <a>{i.title}</a>
+            </li>
+          ) : (
+            <li key={index}>
+              <Link to={i.link}>{i.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
+
+Breadcrumb.propTypes = {
+  links: PropTypes.array.isRequired,
+};
