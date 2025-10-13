a0539ec713fabaa76807b1bb0a4a51e1d8fad3d7
Roman Rachkov <pakycb84@gail.com>
2025-10-13 14:44:15 +1000

    [YOL-88] fix: поправлен секондбар

--- client/src/components/SecondMenu.jsx
+++ client/src/components/SecondMenu.jsx
@@ -1,71 +1,84 @@
 import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { getPayloads } from '../utils/payloads'
+import { getPayloads } from '../utils/payloads';
 
-export default function SeconMenu(){
-    
-    const dologout = () => {
-        logout();
-        navigate('/');
-    };
+export default function SeconMenu() {
+  const dologout = () => {
+    logout();
+    navigate('/');
+  };
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    
-    const { isAuthenticated, logout } = useAuth();
-    
-    const savedUser = getPayloads();    
-    const userRole = savedUser ? savedUser.role : 'unknown'; 
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    console.log('savedUser', savedUser)
-    
-    const navTo = {
-        'company':'/cp/company',
-        'designer':'/cp/designer',
-        'administrator':'/cp/yolk-admin',
-        'unknown':'/',
-    } 
-    
-    return(
-        <div className="second-menu">
+  const { isAuthenticated, logout } = useAuth();
 
-            {
-                isAuthenticated ? (
-                    <div className="second-menu-private">
-                        <div className="level">
-                            <div className="level-item">
-                                <div className="tabs is-boxed ">                
-                                    <ul>
-                                        <li><a alt="Чаты" href="#"><i className="fa-regular fa-comment"></i></a></li>                                        
-                                        <li><a alt="Сообщения системы" href="#"><i className="fa-regular fa-bell"></i></a></li>
+  const savedUser = getPayloads();
+  const userRole = savedUser ? savedUser.role : 'unknown';
 
-                                        <li className={location.pathname === navTo[userRole] ? "is-active" : ""}>
-                                            <NavLink to={navTo[userRole]}><i className="fa-regular fa-user"></i></NavLink>
-                                        </li>
+  const navTo = {
+    company: '/cp/company',
+    designer: '/cp/designer',
+    administrator: '/cp/yolk-admin',
+    unknown: '/',
+  };
 
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="desktop-only">
-                            <div className="level-item">
-                                <button className="button"  onClick={dologout}>                            
-                                    <span><i className="fa-solid fa-arrow-right-from-bracket"></i></span>                
-                                </button>                        
-                            </div>
-                            </div>
-                        </div>
-                    </div> 
-                ):(
-                    <div className="second-menu-public">
-                        <div className="tabs is-right is-boxed ">                
-                        <ul>                          
-                            <li className={location.pathname === "/login" ? "is-active" : ""}>
-                            <NavLink to="/login" end className="nav-link">Вход / Регистрация</NavLink></li>
-                        </ul>
-                        </div>
-                    </div>
-                )
-            }
-    </div>          
-    )
+  return (
+    <div className="second-menu">
+      {isAuthenticated ? (
+        <div className="second-menu-private">
+          <div className="level">
+            <div className="level-item">
+              <div className="tabs is-boxed ">
+                <ul>
+                  {/*<li>*/}
+                  {/*  <a alt="Чаты" href="#">*/}
+                  {/*    <i className="fa-regular fa-comment" />*/}
+                  {/*  </a>*/}
+                  {/*</li>*/}
+                  {/*<li>*/}
+                  {/*  <a alt="Сообщения системы" href="#">*/}
+                  {/*    <i className="fa-regular fa-bell" />*/}
+                  {/*  </a>*/}
+                  {/*</li>*/}
+
+                  <li
+                    className={
+                      location.pathname === navTo[userRole] ? 'is-active' : ''
+                    }
+                  >
+                    <NavLink to={navTo[userRole]}>
+                      <i className="fa-regular fa-user mr-2" />
+                      Кабинет
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="desktop-only">
+              <div className="level-item">
+                <button className="button" onClick={dologout}>
+                  <span>
+                    <i className="fa-solid fa-arrow-right-from-bracket" />
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="second-menu-public">
+          <div className="tabs is-right is-boxed ">
+            <ul>
+              <li className={location.pathname === '/login' ? 'is-active' : ''}>
+                <NavLink to="/login" end className="nav-link">
+                  Вход / Регистрация
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
